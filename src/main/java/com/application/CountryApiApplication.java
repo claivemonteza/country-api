dc5657ea7494c05722945d210f04f542e5a4f028
Claive Monteza <claivemonteza@outlook.com>
2021-10-27 20:36:10 +0200

    Update configuration

--- src/main/java/com/application/CountryApiApplication.java
+++ src/main/java/com/application/CountryApiApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CountryApiApplication {
@@ -16,7 +18,17 @@ public class CountryApiApplication {
 		SpringApplication.run(CountryApiApplication.class, args);
 	}
 	
+	
 	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void AddCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+	
+/*	@Bean
 	public CorsFilter corsFilter() {
 		
 		CorsConfiguration config = new CorsConfiguration();
@@ -32,7 +44,7 @@ public class CountryApiApplication {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
-	}
+	}*/
 
 	
 
