05a7c4ba3711ea30167a8ebb4857391e79b63bb8
Claive Monteza <claivemonteza@outlook.com>
2021-11-08 14:16:03 +0200

    Change permitions

--- src/main/java/com/application/security/WebSecurityConfig.java
+++ src/main/java/com/application/security/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		corsConfiguration.setAllowCredentials(true);
 		
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
+		source.registerCorsConfiguration("/**", corsConfiguration.applyPermitDefaultValues());
 		return new CorsFilter(source);
 	}
 }
