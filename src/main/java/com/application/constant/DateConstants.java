a082aafff66ce8d4e7f0851020354d4b218e0431
Claive Monteza <claivemonteza@outlook.com>
2021-11-11 13:37:26 +0200

    Add Constant

---
+++ src/main/java/com/application/constant/DateConstants.java
@@ -0,0 +1,8 @@
+package com.application.constant;
+
+public class DateConstants {
+	
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
+	public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+}
